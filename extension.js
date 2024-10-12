1fafdcb309805e4dc3030c370c9f7ca0d1e6ff6d
Kaleid Scoper <68382243+KaleidScoper@users.noreply.github.com>
2024-10-12 21:55:15 +0800

    适配c文件
    注意！此版本将清除包含注释的行内所有内容！有待修改！

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const editor = vscode.window.activeTextEditor;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -16,8 +17,39 @@ function activate(context) {
 	});
 
 	const clear = vscode.commands.registerCommand('vscplugin-comments-cleaner.commentsclear', function () {
-		;
-		vscode.window.showInformationMessage('Cleaned.');
+		if (1) {
+            const document = editor.document;
+            const languageId = document.languageId;
+			const text = document.getText();
+            const lines = text.split('\n');
+
+            // 检查文档类型
+            if (languageId === 'c') {
+                vscode.window.showInformationMessage('Cleaning comments for C documents!');
+				editor.edit(editBuilder => {
+					lines.forEach((line, index) => {
+						if (line.includes('//')) {
+							const start = new vscode.Position(index, 0);
+							const end = new vscode.Position(index, line.length);
+							const range = new vscode.Range(start, end);
+							editBuilder.delete(range);
+						}
+					});
+				}).then(success => {
+					if (success) {
+						vscode.window.showInformationMessage('Comments removed successfully!');
+					} else {
+						vscode.window.showErrorMessage('Failed to remove comments.');
+					}
+				});
+            } else if (languageId === 'html') {
+				vscode.window.showInformationMessage('Not for HTML documents!');
+			} else if (languageId === 'php') {
+				;
+			} else {
+				;
+			}
+        }
 	});
 
 	context.subscriptions.push(disposable);
